6865f3d01ed473bba3488a24549d0c21e09ece3e
Iain Merrick <iain@iainmerrick.com>
2017-08-06 23:34:09 -0500

    Add my own damn Assert method. Move it and some other System extensions into a new Utils project, so that Logic can use it

---
+++ Assert.cs
@@ -0,0 +1,16 @@
+﻿﻿using System.Diagnostics;
+
+namespace Utils
+{
+	public static class Assert
+	{
+		public static void That(bool condition, string message = "Assertion failed!")
+		{
+			Debug.Assert(condition, message);
+			if (!condition)
+			{
+				Debugger.Break();
+			}
+		}
+	}
+}
---
+++ Collections.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+namespace Utils
+{
+	public static class Collections
+	{
+		public static bool IsEmpty(this ICollection collection)
+		{
+			return collection.Count == 0;
+		}
+	}
+}
