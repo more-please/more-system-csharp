6acc7084a4b498eb2d439f922d9149868038fc85
Iain Merrick <iain@iainmerrick.com>
2017-08-10 15:23:29 -0500

    JsonReader / JsonWriter

---
+++ JsonReader.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils
+{
+	public class JsonReader
+	{
+		private readonly string _str;
+		private readonly int _max;
+		private int _i;
+
+		// ---------------------------------------------------------------------
+		// High-level API
+
+		public static object Read(string str)
+		{
+			try
+			{
+				return new JsonReader(str).Read();
+			}
+			catch (FormatException)
+			{
+				// TODO: log error
+				return null;
+			}
+		}
+
+		public JsonReader(string str, int startIndex = 0)
+		{
+			_str = str;
+			_max = str.Length;
+			_i = startIndex;
+		}
+
+		public object Read()
+		{
+			Trim();
+			if (AtEnd)
+			{
+				Fail("unexpected end of input");
+			}
+			char c = Peek;
+			if (c == '"')
+			{
+				return ReadString();
+			}
+			else if (c == '[')
+			{
+				return ReadArray();
+			}
+			else if (c == '{')
+			{
+				return ReadDict();
+			}
+			else if (c == '-' || char.IsDigit(c))
+			{
+				return ReadNumber();
+			}
+			else if (c == 't' && Maybe("true"))
+			{
+				return true;
+			}
+			else if (c == 'f' && Maybe("false"))
+			{
+				return false;
+			}
+			else if (c == 'n' && Maybe("null"))
+			{
+				return null;
+			}
+			else
+			{
+				Fail($"unexpected char: '{c}'");
+				return null;
+			}
+		}
+
+		// ---------------------------------------------------------------------
+		// Parse functions for JSON types
+
+		public object ReadNumber()
+		{
+			double result = Sign() * Decimal();
+
+			// Return an int if we can
+			if (result >= Int32.MinValue && result <= Int32.MaxValue)
+				if (Peek != '.' && Peek != 'e' && Peek != 'E')
+					return (int)result;
+
+			if (Maybe('.'))
+			{
+				int fracPos = _i;
+				long frac = Decimal();
+				result += frac / Math.Pow(10, _i - fracPos);
+			}
+			if (Maybe('e') || Maybe('E'))
+			{
+				long exp = Sign() * Decimal();
+				result *= Math.Pow(10, exp);
+			}
+			return result;
+		}
+
+		public string ReadString()
+		{
+			StringBuilder result = new StringBuilder();
+			Expect('"');
+			while (!AtEnd)
+			{
+				char c = Pop();
+				if (c == '"')
+				{
+					return result.ToString();
+				}
+				if (c == '\\')
+				{
+					c = Pop();
+					switch (c)
+					{
+						case 'b': c = '\b'; break;
+						case 'f': c = '\f'; break;
+						case 'n': c = '\n'; break;
+						case 'r': c = '\r'; break;
+						case 't': c = '\t'; break;
+						case 'u': c = Unicode(); break;
+						default: break;
+					}
+				}
+				result.Append(c);
+			}
+			Fail("unterminated string");
+			return null;
+		}
+
+		public IList<object> ReadArray()
+		{
+			List<object> result = new List<object>();
+			Expect('[');
+			Trim();
+			if (Maybe(']'))
+			{
+				return result;
+			}
+			do
+			{
+				object item = Read();
+				result.Add(item);
+				Trim();
+			}
+			while (Maybe(','));
+			Expect(']');
+			return result;
+		}
+
+		public IDictionary<string, object> ReadDict()
+		{
+			var result = new Dictionary<string, object>();
+			Expect('{');
+			Trim();
+			if (Maybe('}'))
+			{
+				return result;
+			}
+			do
+			{
+				Trim();
+				string key = ReadString();
+				Trim();
+				Expect(':');
+				object val = Read();
+				result.Add(key, val);
+				Trim();
+			}
+			while (Maybe(','));
+			Expect('}');
+			return result;
+		}
+
+		// ---------------------------------------------------------------------
+		// Intermediate parsing constructs
+
+		private char Unicode()
+		{
+			int result = 0;
+			for (int i = 0; i < 4; ++i)
+			{
+				if (AtEnd)
+				{
+					Fail("Unterminated Unicode escape");
+				}
+				char c = Pop();
+				if (c >= '0' && c <= '9')
+				{
+					result = (16 * result) + (c - '0');
+				}
+				else if (c >= 'a' && c <= 'f')
+				{
+					result = (16 * result) + (c - 'a' + 10);
+				}
+				else if (c >= 'A' && c <= 'F')
+				{
+					result = (16 * result) + (c - 'A' + 10);
+				}
+				else
+				{
+					Fail($"Expected hex digit");
+				}
+			}
+			return (char)result;
+		}
+
+		private int Sign()
+		{
+			if (Maybe('-')) return -1;
+			if (Maybe('+')) return 1;
+			return 1;
+		}
+
+		private long Decimal()
+		{
+			long result = 0;
+			while (!AtEnd && Peek >= '0' && Peek <= '9')
+				result = result * 10 + Pop() - '0';
+			return result;
+		}
+
+		private void Expect(string expected)
+		{
+			if (!Maybe(expected))
+				Fail($"Expected '{expected}'");
+		}
+
+		private void Expect(char expected)
+		{
+			if (!Maybe(expected))
+				Fail($"Expected '{expected}'");
+		}
+
+		// ---------------------------------------------------------------------
+		// Low-level parse functions
+
+		private bool AtEnd => _i >= _max;
+		private char Peek => _str[_i];
+
+		private string Context => _str.Substring(Math.Max(0, _i - 10));
+		private string Rest => _str.Substring(_i);
+
+		private char Pop()
+		{
+			return _str[_i++];
+		}
+
+		private bool Maybe(string maybe)
+		{
+			for (int j = 0; j < maybe.Length; ++j)
+				if (_i + j >= _max || _str[_i + j] != maybe[j])
+					return false;
+			_i += maybe.Length;
+			return true;
+		}
+
+		private bool Maybe(char maybe)
+		{
+			if (_i >= _max) return false;
+			if (_str[_i] != maybe) return false;
+			++_i;
+			return true;
+		}
+
+		private void Trim()
+		{
+			while (_i < _max && char.IsWhiteSpace(_str, _i))
+				++_i;
+		}
+
+		private void Fail(string message)
+		{
+			throw new FormatException($"JSON parse failed at index {_i}: {message}");
+		}
+	}
+}
---
+++ JsonWriter.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System;
+
+namespace Utils
+{
+	public static class JsonWriter
+	{
+		public static string Write(object obj)
+		{
+			try
+			{
+				StringBuilder b = new StringBuilder();
+				Write(obj, b);
+				return b.ToString();
+			}
+			catch (ArgumentException)
+			{
+				// TODO: log error somewhere?
+				return null;
+			}
+		}
+
+		public static void Write(object obj, StringBuilder output)
+		{
+			if (obj == null)
+			{
+				output.Append("null");
+			}
+			else if (obj is string)
+			{
+				WriteString(obj as string, output);
+			}
+			else if (obj is Int16 || obj is Int32 || obj is Int64 || obj is Enum)
+			{
+				WriteInt(Convert.ToInt64(obj), output);
+			}
+			else if (obj is Single || obj is Double || obj is Decimal)
+			{
+				WriteFloat(Convert.ToDouble(obj), output);
+			}
+			else if (obj is bool)
+			{
+				output.Append((bool)obj ? "true" : "false");
+			}
+			else if (obj is IDictionary)
+			{
+				WriteDict(obj as IDictionary, output);
+			}
+			else if (obj is IEnumerable<KeyValuePair<string, object>>)
+			{
+				WriteDict(obj as IEnumerable<KeyValuePair<string, object>>, output);
+			}
+			else if (obj is IEnumerable)
+			{
+				WriteArray(obj as IEnumerable, output);
+			}
+			else
+			{
+				throw new ArgumentException($"Failed to write JSON from object: ${obj}");
+			}
+		}
+
+		public static void WriteInt(long i, StringBuilder output)
+		{
+			output.Append(i.ToString("D", CultureInfo.InvariantCulture));
+		}
+
+		public static void WriteFloat(double f, StringBuilder output)
+		{
+			output.Append(f.ToString("R", CultureInfo.InvariantCulture));
+		}
+
+		private const string _hexDigits = "0123456789ABCDEF";
+
+		public static void WriteString(string s, StringBuilder output)
+		{
+			output.Append('"');
+			foreach (char c in s)
+			{
+				if (c == '\\')
+				{
+					output.Append("\\\\");
+				}
+				else if (c == '"')
+				{
+					output.Append("\\\"");
+				}
+				else if (c < 32)
+				{
+					output.Append("\\u00");
+					output.Append(_hexDigits[c / 16]);
+					output.Append(_hexDigits[c % 16]);
+				}
+				else
+				{
+					output.Append(c);
+				}
+			}
+			output.Append('"');
+		}
+
+		public static void WriteDict(IDictionary dict, StringBuilder output)
+		{
+			output.Append('{');
+			var e = dict.GetEnumerator();
+			if (e.MoveNext())
+			{
+				WriteString(e.Key.ToString(), output);
+				output.Append(':');
+				Write(e.Value, output);
+				while (e.MoveNext())
+				{
+					output.Append(',');
+					WriteString(e.Key.ToString(), output);
+					output.Append(':');
+					Write(e.Value, output);
+				}
+			}
+			output.Append('}');
+		}
+
+		public static void WriteDict(IEnumerable<KeyValuePair<string, object>> dict, StringBuilder output)
+		{
+			output.Append('{');
+			var e = dict.GetEnumerator();
+			if (e.MoveNext())
+			{
+				WriteString(e.Current.Key as string, output);
+				output.Append(':');
+				Write(e.Current.Value, output);
+				while (e.MoveNext())
+				{
+					output.Append(',');
+					WriteString(e.Current.Key as string, output);
+					output.Append(':');
+					Write(e.Current.Value, output);
+				}
+			}
+			output.Append('}');
+		}
+
+		public static void WriteArray(IEnumerable array, StringBuilder output)
+		{
+			output.Append('[');
+			var e = array.GetEnumerator();
+			if (e.MoveNext())
+			{
+				Write(e.Current, output);
+				while (e.MoveNext())
+				{
+					output.Append(',');
+					Write(e.Current, output);
+				}
+			}
+			output.Append(']');
+		}
+	}
+}
