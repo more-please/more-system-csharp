a857b547c5174e49b13234df68192fe1e7391738
Iain Merrick <iain@iainmerrick.com>
2017-08-10 15:25:44 -0500

    Threading extensions -- Action.ToMainThread, Task.ReturnToMain

---
+++ Actions.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Concurrent;
+using System;
+using System.Threading.Tasks;
+
+namespace Utils
+{
+	public static class Actions
+	{
+		private static readonly ConcurrentQueue<Action> _workQueue = new ConcurrentQueue<Action>();
+
+		public static Action<Task> ToMainThread(this Action action)
+		{
+			return delegate (Task task)
+			{
+				_workQueue.Enqueue(() => action());
+			};
+		}
+
+		public static Action<Task<T>> ToMainThread<T>(this Action<T> action)
+		{
+			return delegate (Task<T> task)
+			{
+				_workQueue.Enqueue(() => action(task.Result));
+			};
+		}
+
+		public static void RunMainThreadActions()
+		{
+			Action action;
+			while (_workQueue.TryDequeue(out action))
+			{
+				action();
+			}
+		}
+	}
+}
---
+++ Tasks.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using System;
+
+namespace Utils
+{
+	public static class Tasks
+	{
+		public static Task ReturnToMain(this Task task, Action action)
+		{
+			return task.ContinueWith(action.ToMainThread());
+		}
+
+		public static Task ReturnToMain<T>(this Task<T> task, Action<T> action)
+		{
+			return task.ContinueWith(action.ToMainThread<T>());
+		}
+	}
+}
