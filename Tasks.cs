8b58e24d93735754a20ad669532cbc867b4430bb
Iain Merrick <iain@iainmerrick.com>
2017-08-10 21:55:48 -0500

    Make Tasks work queue thread-local

--- Tasks.cs
+++ Tasks.cs
@@ -1,36 +1,39 @@
-﻿using System.Threading.Tasks;
-using System;
+﻿using System;
 using System.Collections.Concurrent;
+using System.Threading.Tasks;
 
 namespace Utils
 {
 	public static class Tasks
 	{
-		private static readonly ConcurrentQueue<Action> _workQueue = new ConcurrentQueue<Action>();
-
 		public static Task ContinueHere(this Task task, Action action)
 		{
+			var actions = _actions;
 			return task.ContinueWith((Task t) =>
 			{
-				_workQueue.Enqueue(() => action());
+				actions.Enqueue(() => action());
 			});
 		}
 
 		public static Task ContinueHere<T>(this Task<T> task, Action<T> action)
 		{
+			var actions = _actions;
 			return task.ContinueWith((Task<T> t) =>
 			{
-				_workQueue.Enqueue(() => action(t.Result));
+				actions.Enqueue(() => action(t.Result));
 			});
 		}
 
 		public static void Continue()
 		{
 			Action action;
-			while (_workQueue.TryDequeue(out action))
+			while (_actions.TryDequeue(out action))
 			{
 				action();
 			}
 		}
+
+		[ThreadStatic]
+		private static readonly ConcurrentQueue<Action> _actions = new ConcurrentQueue<Action>();
 	}
 }
