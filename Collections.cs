0e42828925a1886f125ee62590c7f16741c15011
Iain Merrick <iain@iainmerrick.com>
2017-08-14 12:09:18 -0500

    Fix a weird generics class in Collections.cs

--- Collections.cs
+++ Collections.cs
@@ -6,7 +6,16 @@ namespace Utils
 {
 	public static class Collections
 	{
-		public static bool IsEmpty(this ICollection collection)
+		public static bool IsEmpty<T>(this ICollection<T> collection)
+		{
+			return collection.Count == 0;
+		}
+
+		// Bizarrely, Queue<T> implements ICollection but not ICollection<T>.
+		// We can't overload on ICollection because many collections also
+		// implement ICollection<T>, IList<T> etc, so the overload would be
+		// ambiguous. So just treat Queue as a special case.
+		public static bool IsEmpty<T>(this Queue<T> collection)
 		{
 			return collection.Count == 0;
 		}
