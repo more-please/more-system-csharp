9f23024df38d0f2b72d2fe0e236abc949253bb52
Iain Merrick <iain@iainmerrick.com>
2017-08-21 10:35:28 -0500

    Add SemaphoreSlims.DisposableWaitAsync

---
+++ More.System/Disposable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace More.System
+{
+	public class Disposable : IDisposable
+	{
+		private readonly Action _action;
+		private int _count = 0;
+
+		public Disposable(Action action)
+		{
+			_action = action.NotNull();
+		}
+
+		public void Dispose()
+		{
+			if (Interlocked.Increment(ref _count) > 1)
+				throw new ObjectDisposedException("More.System.Disposable");
+			_action();
+		}
+	}
+}
---
+++ More.System/SemaphoreSlims.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace More.System
+{
+	public static class SemaphoreSlims
+	{
+		public static async Task<IDisposable> DisposableWaitAsync(this SemaphoreSlim sem)
+		{
+			await sem.WaitAsync();
+			return new Disposable(() => sem.Release());
+		}
+	}
+}
